6c779ad32cc52b52371dc76c529ce49c3bc4f5c4
aattak <aattak@e2r13p12.1337.ma>
2023-12-14 11:16:30 +0100

    add c03

---
+++ c03/ex04/ft_strstr.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aattak <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/26 07:49:18 by aattak            #+#    #+#             */
+/*   Updated: 2023/08/26 16:24:46 by aattak           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	if (!ft_strlen(to_find))
+		return (str);
+	while (str[i] != '\0')
+	{
+		while (str[i + j] == to_find[j] && str[i + j] != '\0')
+			j++;
+		if (to_find[j] == '\0')
+			return (&str[i]);
+		j = 0;
+		i++;
+	}
+	return (0);
+}
---
+++ c03/ex05/ft_strlcat.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aattak <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/29 10:57:15 by aattak            #+#    #+#             */
+/*   Updated: 2023/09/04 15:42:43 by aattak           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlen(char *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (*str)
+	{
+		str++;
+		i++;
+	}
+	return (i);
+}
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	j;
+	unsigned int	ldst;
+	unsigned int	lsrc;
+
+	ldst = ft_strlen(dest);
+	lsrc = ft_strlen(src);
+	if (size <= ldst)
+		return (lsrc + size);
+	i = 0;
+	j = ldst;
+	while (src[i] != '\0' && j < size - 1)
+	{
+		dest[j] = src[i];
+		i++;
+		j++;
+	}
+	dest[j] = '\0';
+	return (lsrc + ldst);
+}
